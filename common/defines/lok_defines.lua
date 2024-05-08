e708119d2a4e1d19d8ee18a73b8bc249eedd4188
Weeny-Pouchkinn <51378512+Weeny-Pouchkinn@users.noreply.github.com>
2024-05-08 22:51:32 +0200

    WIP Pawris + Peacedeal stuff

---
+++ common/defines/lok_defines.lua
@@ -0,0 +1,5 @@
+NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 8.053
+NDefines.NCountry.MIN_STABILITY = -1.0
+NDefines.NCountry.MAX_STABILITY = 1.0
+NDefines.NCountry.MIN_WAR_SUPPORT = -1.0
+NDefines.NCountry.MAX_WAR_SUPPORT = 1.0
